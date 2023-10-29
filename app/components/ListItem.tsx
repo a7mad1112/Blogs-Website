78eeb867b00a1b99ff5432888be1218b5dcd3631
a7mad1112 <ahmalawneh79@gmail.com>
2023-10-29 14:28:17 +0200

    Fix light mode font-color

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -10,13 +10,13 @@ export default function ListItem({ post }: Props) {
   return (
     <li className="mt-4 text-2xl dark:text-white-90">
       <Link
-        className="underline text-white/90 hover:text-white"
+        className="underline dark:text-white/90 dark:hover:text-white"
         href={`/posts/${id}`}
       >
         {title}
       </Link>
       <br />
-      <p className="text-sm mt-1 text-white/90 hover:text-white">{formattedDate}</p>
+      <p className="text-sm mt-1 dark:text-white/90 dark:hover:text-white">{formattedDate}</p>
     </li>
   );
 }
